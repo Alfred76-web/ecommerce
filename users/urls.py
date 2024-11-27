f15f6fef97a597ac7e805322aaf0305b509f61f9
Alfred76-web <alfred.njaku@strathmore.edu>
2024-11-27 16:52:31 +0300

    #users_final

---
+++ users/urls.py
@@ -0,0 +1,8 @@
+# users/urls.py
+from django.urls import path
+from . import views  # Import views from the current app
+
+
+urlpatterns = [
+    path('', views.home, name='home'),  # Map the home view to the root URL
+]
